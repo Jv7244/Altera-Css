65493a6cc3b8b4747043f28fa1b7f51c1f5c6b81
Jv7244 <victor.jv7244@gmail.com>
2022-06-21 00:54:40 -0300

    master

---
+++ script.js
@@ -0,0 +1,14 @@
+let m = document.getElementById('main')
+
+function mostra1(){
+    console.log("tudo certo 1")
+    m.style.cssText = "background-color: yellow"
+}
+function mostra2(){
+    console.log("tudo certo 2")
+    m.style.cssText = "background-color: pink"
+}
+function mostra3(){
+    console.log("tudo certo 3")
+    m.style.cssText = "background-color: blue"
+}
